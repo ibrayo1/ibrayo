edb7916ffd58bca2a28ce3eb0b649c26cd5de673
ibrayo1 <ibrayo1128@gmail.com>
2020-07-12 18:48:07 -0500

    added form validation to contact form

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,7 +3,7 @@ import AppNavbar from './AppNavbar';
 import Home from './Home';
 import About from './About';
 import Portfolio from './Portfolio';
-import { Contact } from './Contact';
+import Contact from './Contact';
 import Footer from './Footer';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,22 +1,43 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import Axios from "axios";
-import { Alert, Container, Card, CardBody, Form, FormGroup, Label, Input, Row, Col, Button } from 'reactstrap';
+import { AvGroup, AvForm, AvInput } from 'availity-reactstrap-validation';
+import { Alert, Container, Card, CardBody, Label, Row, Col, Button } from 'reactstrap';
 
-export const Contact = () => {
+class Contact extends Component {
 
-    const initialInputState = { name: "", email: "", message: "" };
-    const [newMessage, setNewMessage] = useState(initialInputState);
-    const [visible, setVisible] = useState(false);
+    constructor(props) {
+        super(props);
 
-    const { name, email, message } = newMessage;
+        this.handleValidSubmit = this.handleValidSubmit.bind(this);
+        this.handleInvalidSubmit = this.handleInvalidSubmit.bind(this);
+        this.onDismiss = this.onDismiss.bind(this);
 
-    const handleInputChange = e => {
-        setNewMessage({ ...newMessage, [e.target.name]: e.target.value });
-    };
-    
-    const onDismiss = () => setVisible(false);
+        this.state = {name: false, email: false, message: false, visible: false};
+    }
 
-    const sendMessage = e => {
+    onDismiss(){
+        this.setState({visible: false});
+    }
+
+    handleValidSubmit(event, values) {
+        this.sendMessage(values);
+    }
+
+    handleInvalidSubmit(event, errors, values) {
+        this.setState({
+            name: values.name, 
+            email: values.email, 
+            message: values.message, 
+            error: true
+        });
+        console.log("invalid input received");
+    }
+
+    sendMessage(values){
+        const name = values.name;
+        const email = values.email;
+        const message = values.message;
+
         Axios({
           method: "POST",
           url: "http://localhost:5000/send",
@@ -26,59 +47,64 @@ export const Contact = () => {
           }
         }).then(res => {
           if (res.data.msg === "suc") {
-            setVisible(true); // show success alert
-            setNewMessage(initialInputState);
+            console.log('SUCCESS');
+            this.setState({visible: true});
+            this.form && this.form.reset();
           } else {
-            console.log("FAILURE");
+            console.log("FAILURE TO SEND");
           }
         });
     };
  
-    return(
-        <div>
-            <svg preserveAspectRatio="none" viewBox="0 0 810 102" height="75" width="100%" className="gradient-bg" >
-                <path d="M 0 0 Q 129 25 260 35 L 309 74 L 509 74 L 560 37 Q 689 26 809 0 " fill="white" stroke="white" strokeWidth="3"></path>
-            </svg>
-
-            <section id="contact">
-            
-                <h1 className="section-title">CONTACT</h1>
-                <div className="underline"></div>
-
-                <Container >
-                    <Col lg={{size: "10", offset: "1"}}>
-                        <Card className="contactCard p-3" >
-                            <CardBody>
-                                <Row>
-                                    <Col lg={{size: "5"}} style={{textAlign: 'center'}}>
-                                        <img src={require('../images/mail.png')} className="img-fluid" alt="letter" style={{padding: 15}}/>
-                                    </Col>
-                                    <Col lg={{size: "7"}}>
-                                        <Form style={{color: '#000000'}} action="/contact" method="POST">
-                                            <FormGroup>
-                                                <Input type="text" name="name" onChange={handleInputChange} value={name} id="name" required/>
-                                                <Label for="name">Name</Label>
-                                            </FormGroup>
-                                            <FormGroup>
-                                                <Input type="email" name="email" onChange={handleInputChange} value={email} id="email" required/>
-                                                <Label for="name">Email</Label>
-                                            </FormGroup>
-                                            <FormGroup>
-                                                <Input type="textarea" rows="5" name="message" onChange={handleInputChange} value={message} id="message" />
-                                                <Label for="message">Message</Label>
-                                            </FormGroup>
-                                            <Alert color="success" isOpen={visible} toggle={onDismiss}>
-                                                Your message was sent successfully, Thanks!
-                                            </Alert>
-                                            <Button className="messagebtn" onClick={sendMessage}>SEND</Button>
-                                        </Form>
-                                    </Col>
-                                </Row>
-                            </CardBody>
-                        </Card>
-                    </Col>
-                </Container>
-            </section>
-        </div>
-    );
+    render(){
+        return(
+            <div>
+                <svg preserveAspectRatio="none" viewBox="0 0 810 102" height="75" width="100%" className="gradient-bg" >
+                    <path d="M 0 0 Q 129 25 260 35 L 309 74 L 509 74 L 560 37 Q 689 26 809 0 " fill="white" stroke="white" strokeWidth="3"></path>
+                </svg>
+    
+                <section id="contact">
+                
+                    <h1 className="section-title">CONTACT</h1>
+                    <div className="underline"></div>
+    
+                    <Container >
+                        <Col lg={{size: "10", offset: "1"}}>
+                            <Card className="contactCard p-3" >
+                                <CardBody>
+                                    <Row>
+                                        <Col lg={{size: "5"}} style={{textAlign: 'center'}}>
+                                            <img src={require('../images/mail.png')} className="img-fluid" alt="letter" style={{padding: 15}}/>
+                                        </Col>
+                                        <Col lg={{size: "7"}}>
+                                            <AvForm style={{color: '#000000'}} onValidSubmit={this.handleValidSubmit} onInvalidSubmit={this.handleInvalidSubmit} ref={c => (this.form = c)}>
+                                                <AvGroup>
+                                                    <AvInput type="text" name="name" id="name" required/>
+                                                    <Label for="name">Name</Label>
+                                                </AvGroup>
+                                                <AvGroup>
+                                                    <AvInput type="email" name="email" id="email" required/>
+                                                    <Label for="name">Email</Label>
+                                                </AvGroup>
+                                                <AvGroup>
+                                                    <AvInput type="textarea" rows="5" name="message" id="message" />
+                                                    <Label for="message">Message</Label>
+                                                </AvGroup>
+                                                <Alert color="success" isOpen={this.state.visible} toggle={this.onDismiss}>
+                                                    Your message was sent successfully, Thanks!
+                                                </Alert>
+                                                <Button className="messagebtn" >SEND</Button>
+                                            </AvForm>
+                                        </Col>
+                                    </Row>
+                                </CardBody>
+                            </Card>
+                        </Col>
+                    </Container>
+                </section>
+            </div>
+        );
+    }
 }
+
+export default Contact;
