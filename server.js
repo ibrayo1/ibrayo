d2c6208feace083f6dc6505f7cf6511f53a66ce1
ibrayo1 <ibrayo1128@gmail.com>
2020-09-20 16:11:41 -0500

    rdy for prod

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const server = require('http').createServer(app);
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 
 app.use(cors());
 
@@ -21,6 +22,16 @@ mongoose
 // bring in routes
 app.use('/', require('./routes/api/index'));
 app.use('/api', require('./routes/api/projects'));
+
+// Serve static assets in production
+if(process.env.NODE_ENV == 'production'){
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const port = process.env.PORT || 5000;
 
